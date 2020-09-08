a157fa795e0cea1b1b8bcffb5f0a1942945f7cce
Tucker McCoy <tuckermmccoy@gmail.com>
2020-09-08 11:06:21 -0400

    Added optional locailty flag for specific regions in Virginia

--- src/commands/cmd_total.py
+++ src/commands/cmd_total.py
@@ -12,13 +12,35 @@ from src.models import VaCovid
 
 
 @click.command("total", short_help="List the Virginia Covid-19 case data.")
+@click.option(
+    "--locality", "-l", required=False, help="Virginia Locality.",
+)
 @pass_environment
-def cli(ctx):
+def cli(ctx, locality):
     """Data visualization of all covid-19 va data."""
 
     ctx.log("Total Covid-19 Virginia Cases...")
 
-    values = db_session.query(func.sum(VaCovid.total_cases).label("total_cases"))
+    if locality:
+        values = db_session.query(
+            func.sum(VaCovid.total_cases).label("total_cases")
+        ).filter(VaCovid.locality == locality.upper())
 
-    for _res in values.all():
-        print("Total cases as of today are: {}".format(_res[0]))
+        for _res in values.all():
+            if _res[0] is None:
+                print(
+                    "No available Virginia Covid-19 case data for {}".format(
+                        locality.capitalize()
+                    )
+                )
+            else:
+                print(
+                    "Total cases for {} as of today are: {}".format(
+                        locality.capitalize(), _res[0]
+                    )
+                )
+    else:
+        values = db_session.query(func.sum(VaCovid.total_cases).label("total_cases"))
+
+        for _res in values.all():
+            print("Total cases as of today are: {}".format(_res[0]))
